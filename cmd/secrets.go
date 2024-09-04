b12de41f5aa8e5c84cc3a3a5df635b1dfcbbbc23
R-dVL <rauldel.valle.lima@gmail.com>
2024-09-04 20:55:41 +0200

    Fix yaml disorder

--- cmd/secrets.go
+++ cmd/secrets.go
@@ -1,12 +1,13 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
 // Variable para almacenar la clave que se pasará con la opción --hide
@@ -64,8 +65,8 @@ func processFile(file, clave string) error {
 		return fmt.Errorf("no se pudo leer el archivo %s: %v", file, err)
 	}
 
-	// Unmarshal del contenido YAML en un mapa genérico
-	var content map[interface{}]interface{}
+	// Unmarshal del contenido YAML en un mapa de string a interface{}
+	var content map[string]interface{}
 	err = yaml.Unmarshal(data, &content)
 	if err != nil {
 		return fmt.Errorf("error al parsear el archivo YAML %s: %v", file, err)
@@ -76,11 +77,25 @@ func processFile(file, clave string) error {
 
 	// Si se realizó un cambio, guardar el archivo
 	if changed {
-		newData, err := yaml.Marshal(content)
+		// Convertir el mapa a JSON para preservar el orden
+		jsonData, err := json.Marshal(content)
+		if err != nil {
+			return fmt.Errorf("error al generar JSON para %s: %v", file, err)
+		}
+
+		// Convertir el JSON a YAML
+		var newData interface{}
+		err = json.Unmarshal(jsonData, &newData)
+		if err != nil {
+			return fmt.Errorf("error al parsear JSON para %s: %v", file, err)
+		}
+
+		newYAMLData, err := yaml.Marshal(newData)
 		if err != nil {
 			return fmt.Errorf("error al generar YAML para %s: %v", file, err)
 		}
-		err = os.WriteFile(file, newData, 0644)
+
+		err = os.WriteFile(file, newYAMLData, 0644)
 		if err != nil {
 			return fmt.Errorf("error al escribir el archivo %s: %v", file, err)
 		}
@@ -91,15 +106,13 @@ func processFile(file, clave string) error {
 }
 
 // replaceValue reemplaza el valor de la clave especificada en el mapa y sus submapas
-func replaceValue(content map[interface{}]interface{}, clave string) bool {
+func replaceValue(content map[string]interface{}, clave string) bool {
 	changed := false
 	for k, v := range content {
-		// Verifica si la clave es un string y coincide con la clave que buscamos
-		if key, ok := k.(string); ok && key == clave {
+		if k == clave {
 			content[k] = "secret"
 			changed = true
-		} else if nestedMap, ok := v.(map[interface{}]interface{}); ok {
-			// Si es un mapa anidado, llamar recursivamente
+		} else if nestedMap, ok := v.(map[string]interface{}); ok {
 			if replaceValue(nestedMap, clave) {
 				changed = true
 			}
